00c18d8f4da1ff57757655615c88da754530b00c
daviazevdo <azvd.davi@gmail.com>
2022-10-08 23:00:27 -0300

    atualiza o contexto mas ainda deixa a foto padrão

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -4,15 +4,15 @@ import appContext from './Context';
 
 function Provider({ children }) {
   const [name, setName] = useState('Cristiano');
-  const [description, setDescription] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum justo at aliquam mollis. Etiam eros tellus, blandit eget dui consequat, rutrum sodales diam. Curabitur consequat ipsum ut turpis consequat, a ullamcorper mauris tempus. In in volutpat nunc. Maecenas ac accumsan arcu, nec rhoncus velit. Aliquam vitae ante sodales, semper l');
-  const [attr1, setAttr1] = useState(80);
-  const [attr2, setAttr2] = useState(75);
-  const [attr3, setAttr3] = useState(60);
-  const [attr4, setAttr4] = useState(60);
-  const [attr5, setAttr5] = useState(60);
-  const [attr6, setAttr6] = useState(60);
+  const [description, setDescription] = useState('');
+  const [attr1, setAttr1] = useState(0);
+  const [attr2, setAttr2] = useState(0);
+  const [attr3, setAttr3] = useState(0);
+  const [attr4, setAttr4] = useState(0);
+  const [attr5, setAttr5] = useState(0);
+  const [attr6, setAttr6] = useState(0);
   const [imageUrl, setImageUrl] = useState('https://static.poder360.com.br/2021/08/photo_2021-08-27_15-23-08.jpg');
-  const [isSuperTrunfo, setIsSuperTrunfo] = useState(true);
+  const [isSuperTrunfo, setIsSuperTrunfo] = useState(false);
   const [rarity, setRarity] = useState('Raro');
 
   const context = {
