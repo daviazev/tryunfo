27c4381a9f49e9fc158438d092159e0ac571dce6
daviazevdo <azvd.davi@gmail.com>
2022-10-20 23:17:08 -0300

    adiciona mensagem para caso a largura da tela seja menor que 950 pixels

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from './components/Form';
 import CardPreview from './components/CardPreview';
 
@@ -6,7 +6,35 @@ import logo from './images/logo.svg';
 
 import './styles/App.css';
 
+const MIN_SIZE = 950;
+
 function App() {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > MIN_SIZE);
+
+  useEffect(() => {
+    const xablei = () => {
+      setIsDesktop(window.innerWidth > MIN_SIZE);
+    };
+
+    window.addEventListener('resize', xablei);
+
+    return () => {
+      window.removeEventListener('resize', xablei);
+    };
+  }, []);
+
+  if (!isDesktop) {
+    return (
+      <div className="not-desktop">
+        <h2>Ah não :(</h2>
+        <p>
+          A tela do seu dispositivo é muito pequena e ainda não fizemos uma versão mobile.
+          Use uma tela com a largura maior que 950 pixels para usar o app.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="logo-content">
